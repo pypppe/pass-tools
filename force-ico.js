2ffe2ad9461a363cc2cfa81263f213f550dfaa24
kona-chan <bobdirmanner@gmail.com>
2026-01-17 22:33:12 +0000

    Update force-ico.js

--- force-ico.js
+++ force-ico.js
@@ -1,21 +1,12 @@
-(function () {
+document.addEventListener("DOMContentLoaded", () => {
     const faviconUrl = "https://ptools.astrarune.com/favicon.ico";
 
-    function setFavicon() {
-        document.querySelectorAll("link[rel*='icon']").forEach(el => el.remove());
+    document.querySelectorAll("link[rel*='icon']").forEach(el => el.remove());
 
-        const link = document.createElement("link");
-        link.rel = "icon";
-        link.type = "image/x-icon";
-        link.href = faviconUrl;
+    const link = document.createElement("link");
+    link.rel = "icon";
+    link.type = "image/x-icon";
+    link.href = faviconUrl;
 
-        document.head.appendChild(link);
-    }
-
-    setFavicon();
-
-    window.addEventListener("load", setFavicon);
-
-    const observer = new MutationObserver(setFavicon);
-    observer.observe(document.head, { childList: true });
-})();
+    document.head.appendChild(link);
+});
